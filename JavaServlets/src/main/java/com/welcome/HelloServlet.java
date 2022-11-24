43a5bda9ed4eae122b6a63941d97348f1c0a65ed
Reina-annie <anastasia_dmitrenko@yahoo.co.uk>
2022-11-24 01:42:58 +0000

    first commit

---
+++ JavaServlets/src/main/java/com/welcome/HelloServlet.java
@@ -0,0 +1,44 @@
+package com.welcome;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("Text/html");
+		PrintWriter pw=response.getWriter();
+		
+		String n1 = request.getParameter("name");//will return value
+		//pw.println("Welcome: " + n1);
+		pw.println("<html><body>");
+		pw.println("<h3> Welcome to servlet " + n1 + "</h3>");
+		pw.println("</body></html>");
+	}
+
+}
